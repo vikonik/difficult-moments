e37bb0cbc42123f15168cdd4fe0475c93bc39052
vikonik <vikonik@yandex.ru>
2021-10-04 21:36:23 +0300

    Домашнее задание 3

--- main.cpp
+++ main.cpp
@@ -1,84 +1,74 @@
 /*
-   1 Реализуйте шаблонную функцию Swap, которая принимает два указателя и обменивает местами значения, на которые указывают эти указатели.
-   2 Реализуйте шаблонную функцию SortPointers, которая принимает вектор указателей и сортирует указатели по значениям, на которые они указывают.
-   3 Подсчитайте количество гласных букв в книге “Война и мир”. Для подсчета используйте 4 способа:
-        ○ count_if и find
-        ○ count_if и цикл for
-        ○ цикл for и find
-        ○ 2 цикла for
-
-Замерьте время каждого способа подсчета и сделайте выводы.
-Справка:
-count_if - это алгоритмическая функция из STL, которая принимает 3 параметра: итератор на начало, итератор на конец и унарный предикат (функцию, принимающую один параметр и возвращающую тип bool).
-find - это метод класса string, который возвращает позицию символа (строки), переданного в качестве параметра, в исходной строке. Если символ не найден, то метод возвращает string::npos.
+1 Написать функцию, добавляющую в конец списка вещественных чисел элемент, значение которого равно среднему арифметическому всех его элементов.
+2 Создать класс, представляющий матрицу. Реализовать в нем метод, вычисляющий определитель матрицы.
+3 Реализовать собственный класс итератора, с помощью которого можно будет проитерироваться по диапазону целых чисел в цикле for-range-based.
 
 */
 
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <clocale>
-
+#include <list>
+#include <iterator>
+#include <array>
 #include "Timer.h"
-
 using namespace std;
 
-/*
-Шаблонная фуикция Swap
-*/
-template <typename T>
-void Swap( T *a , T *b){
-    T tmp = *b;
-    *b = *a;
-    *a = tmp;
-}
-
-/*
-Шаблонная фуикция SortPointers
-*/
-template <typename T>
-void SortPointers(vector<T> *a){
-    sort(a->begin(),a->end());
-}
-
-//Русские буквы не воспинимает, поэтому используем латинские
-char vowelLtter[6]={'a','e', 'i', 'o', 'u','y'};
-//Это попытка написать функцию для работы с count_if
-bool isVowelLtter(int i, int num)
-{
-    if (i < num)
-        return true;
-    else
-        return false;
-}
-
-
-
-/*
-Считаем буквы 2 цикла for
-*/
-int usingFor(string *data, int size){
-    Timer timer("for & for");
-    int cnt = 0;
-    string str = "asdfsadf";
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < sizeof(vowelLtter); j++){
-           std::string:find
-            if((*data) == vowelLtter[j]){//vowelLtter[j]
-                cnt++;
-                break;
-            }
-        }
+void addList(int listSize){
+    int sum = 0;
+    list<double>myList;
+    myList.resize(listSize);
+    for(int i = 0; i < listSize; i++){
+        sum += i;//сумма элементов
+        myList.push_back((sum/(i+1)));
     }
-    timer.print();
-    return cnt;
 }
 
 /**/
+class Matrix{
+private:
+static const int matrixSize = 3;
+int result = 0;
+ //   int matrix[matrixSize][matrixSize];
+list<int>line_1 = {1,2,3};
+list<int>line_2 = {4,5,6};
+list<int>line_3 = {7,8,9};
+list<list <int>>matrix = {line_1, line_2, line_3};
+
+
+public :
+    Matrix(){
+
+    }
+
+
+    int calcMatrix(){
+//   return  matrix[0][0]*matrix[1][1]*matrix[2][2] +
+//            matrix[1][0]*matrix[2][1]*matrix[0][2] +
+//            matrix[2][0]*matrix[1][1]*matrix[0][2] -
+//            matrix[0][0]*matrix[2][1]*matrix[1][2] -
+//            matrix[1][0]*matrix[0][1]*matrix[2][2];
+
+//cout << matrix.pop_front();
+
+std::cout << line_1.front() << endl;
+std::cout << line_1.front() << endl;
+std::cout << line_1.front() << endl;
+
+list<int> tmp = matrix.front();
+int a = tmp.front();
+int b =
+for(auto i: line_1)cout << i;
+    }
+};
+
+
+/*
+
+*/
 int main()
 {
+Matrix m;
+m.calcMatrix();
 
-
-
+    cout << "Hello World!" << endl;
     return 0;
 }
