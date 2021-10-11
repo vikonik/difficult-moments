a307d50f03f8fb89d6c9c5c53d6ae7d3dbc192f9
vikonik <vikonik@yandex.ru>
2021-10-11 22:00:28 +0300

    Домашнее задание 5

--- main.cpp
+++ main.cpp
@@ -1,73 +1,35 @@
 /*
-1 Написать функцию, добавляющую в конец списка вещественных чисел элемент, значение которого равно среднему арифметическому всех его элементов.
-2 Создать класс, представляющий матрицу. Реализовать в нем метод, вычисляющий определитель матрицы.
-3 Реализовать собственный класс итератора, с помощью которого можно будет проитерироваться по диапазону целых чисел в цикле for-range-based.
+Создать шаблонную функцию, которая принимает итераторы на начало и конец последовательности слов,
+и выводящую в консоль список уникальных слов (если слово повторяется больше 1 раза, то вывести его надо один раз).
+Продемонстрировать работу функции, передав итераторы различных типов.
 
+Используя ассоциативный контейнер, напишите программу,
+которая будет считывать данные введенные пользователем из стандартного потока ввода и разбивать их на предложения.
+Далее программа должна вывести пользователю все предложения, отсортировав их по длине.
 */
 
 #include <iostream>
-#include <list>
 #include <iterator>
-#include <array>
-#include "Timer.h"
+#include <vector>
+#include <list>
 using namespace std;
 
-void addList(int listSize){
-    int sum = 0;
-    list<double>myList;
-    myList.resize(listSize);
-    for(int i = 0; i < listSize; i++){
-        sum += i;//сумма элементов
-        myList.push_back((sum/(i+1)));
-    }
+
+//Это попытка создать шаблонную функцию
+//шибка: missing 'typename' prior to dependent type name 'vector<T>::iterator'
+//как исправить не знаю
+template <typename T>;
+void show_string( vector<T>::iterator &begin, vector<T>::iteranor &end){
+
 }
 
-/**/
-class Matrix{
-private:
-static const int matrixSize = 3;
-int result = 0;
- //   int matrix[matrixSize][matrixSize];
-list<int>line_1 = {1,2,3};
-list<int>line_2 = {4,5,6};
-list<int>line_3 = {7,8,9};
-list<list <int>>matrix = {line_1, line_2, line_3};
 
 
-public :
-    Matrix(){
-
-    }
 
 
-    int calcMatrix(){
-//   return  matrix[0][0]*matrix[1][1]*matrix[2][2] +
-//            matrix[1][0]*matrix[2][1]*matrix[0][2] +
-//            matrix[2][0]*matrix[1][1]*matrix[0][2] -
-//            matrix[0][0]*matrix[2][1]*matrix[1][2] -
-//            matrix[1][0]*matrix[0][1]*matrix[2][2];
-
-//cout << matrix.pop_front();
-
-std::cout << line_1.front() << endl;
-std::cout << line_1.front() << endl;
-std::cout << line_1.front() << endl;
-
-list<int> tmp = matrix.front();
-int a = tmp.front();
-int b =
-for(auto i: line_1)cout << i;
-    }
-};
-
-
-/*
-
-*/
 int main()
 {
-Matrix m;
-m.calcMatrix();
+
 
     cout << "Hello World!" << endl;
     return 0;
